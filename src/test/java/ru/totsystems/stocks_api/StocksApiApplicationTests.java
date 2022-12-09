be0f7a2fca431de1118933ed32625a3ce1cc52f8
Farmanian Karen <farmanian.k@yandex.ru>
2022-12-09 18:29:53 +0300

    initial commit

---
+++ src/test/java/ru/totsystems/stocks_api/StocksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.totsystems.stocks_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
