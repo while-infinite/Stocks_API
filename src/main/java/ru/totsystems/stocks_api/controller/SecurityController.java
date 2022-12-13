a586a7df73c04f00110f14d2321a5d9d014b53f3
Farmanian Karen <farmanian.k@yandex.ru>
2022-12-13 21:03:49 +0300

    Add swagger

--- src/main/java/ru/totsystems/stocks_api/controller/SecurityController.java
+++ src/main/java/ru/totsystems/stocks_api/controller/SecurityController.java
@@ -1,5 +1,7 @@
 package ru.totsystems.stocks_api.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,7 @@ import ru.totsystems.stocks_api.model.Security;
 import ru.totsystems.stocks_api.service.SecurityService;
 import ru.totsystems.stocks_api.validate.FileValidator;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -22,11 +25,13 @@ import java.util.Map;
 @Controller
 @RequestMapping("/security")
 @RequiredArgsConstructor
+@Api(value = "SecurityController")
 public class SecurityController {
     private final SecurityService securityService;
     private final SecurityMapper mapper;
 
     @PostMapping("/upload")
+    @ApiOperation(value = "Upload files", tags = "addSecurityAndHistory")
     public String addSecurityAndHistory(@RequestParam("collage") MultipartFile[] files, RedirectAttributes attributes) {
         Map<String, FilesDto> validate = FileValidator.validateRequest(files);
         if (validate.containsKey("Please select a file to upload.")) {
@@ -46,12 +51,14 @@ public class SecurityController {
     }
 
     @PutMapping("/updateSecurity")
+    @ApiOperation(value = "Update security", tags = "updateSecurity")
     public String updateSecurity(SecurityDto securityDto) {
         securityService.save(securityDto);
         return "redirect:/security";
     }
 
     @GetMapping("/uploadForm")
+    @ApiOperation(value = "Return upload form", tags = "getUploadForm")
     public ModelAndView getUploadForm() {
         ModelAndView mav = new ModelAndView("upload-files-form");
         mav.addObject("filesDto", new FilesDto());
@@ -59,6 +66,7 @@ public class SecurityController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Return security list form", tags = "getSecurityList")
     public ModelAndView getSecurityList() {
         ModelAndView mav = new ModelAndView("security-list");
         List<Security> securityList = securityService.getSecurityList();
@@ -68,6 +76,7 @@ public class SecurityController {
     }
 
     @PutMapping("/updateForm")
+    @ApiOperation(value = "Return update security form", tags = "getUpdateForm")
     public ModelAndView getUpdateForm(@RequestParam Long securityId) {
         ModelAndView mav = new ModelAndView("update-security-form");
         Security security = securityService.getSecurity(securityId);
@@ -79,6 +88,7 @@ public class SecurityController {
     }
 
     @DeleteMapping("/deleteSecurity")
+    @ApiOperation(value = "Delete security", tags = "deleteSecurity")
     public String deleteSecurity(@RequestParam Long securityId) {
         securityService.deleteSecurity(securityId);
         return "redirect:/security";
